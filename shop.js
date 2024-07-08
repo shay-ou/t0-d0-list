9765dbd8968fee9c93098a982eb007ae36ae988b
Shreya <148710467+shay-ou@users.noreply.github.com>
2024-07-08 22:21:37 +0530

    Update shop.js

--- shop.js
+++ shop.js
@@ -30,4 +30,80 @@ addBtn.addEventListener("click", function () {
     if (inputValue.trim() !== "") {
         push(shoppingListInDB, { text: inputValue, checked: false });
         clearInputField();
-    }})
+    }
+});
+
+onValue(shoppingListInDB, function (snapshot) {
+    clearShoppingList();
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val());
+        itemsArray.forEach((currentItem) => {
+            addToCart(currentItem);
+        });
+    } else {
+        ulEl.innerHTML = "I know you can do it :)";
+    }
+});
+
+function addToCart(item) {
+    let itemID = item[0];
+    let itemValue = item[1];
+
+    if (typeof itemValue.text === 'undefined') {
+        console.error("itemValue.text is undefined for item:", item);
+        return;
+    }
+
+    let newEl = document.createElement("li");
+
+    newEl.style.alignItems = "center";
+    newEl.style.padding = "20px 0";
+    newEl.style.fontFamily = "georgia";
+    newEl.className = "list-item form-check";
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = `checkbox-${itemID}`;
+    checkbox.checked = itemValue.checked;
+    checkbox.className = "form-check-input";
+
+    let label = document.createElement("label");
+    label.setAttribute("for", `checkbox-${itemID}`);
+    label.textContent = itemValue.text;
+    label.className = "form-check-label";
+
+    if (checkbox.checked) {
+        label.style.textDecoration = "line-through";
+    } else {
+        label.style.textDecoration = "none";
+    }
+
+    checkbox.addEventListener("change", function () {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${userUID}/${itemID}`);
+        update(exactLocationOfItemInDB, {
+            checked: checkbox.checked
+        });
+        if (checkbox.checked) {
+            label.style.textDecoration = "line-through";
+        } else {
+            label.style.textDecoration = "none";
+        }
+    });
+
+    newEl.appendChild(checkbox);
+    newEl.appendChild(label);
+
+    ulEl.append(newEl);
+}
+
+del.addEventListener("click", function () {
+    remove(shoppingListInDB);
+});
+
+function clearInputField() {
+    inputEl.value = "";
+}
+
+function clearShoppingList() {
+    ulEl.innerHTML = "";
+}
